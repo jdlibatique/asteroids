83d170f79bb11ee99f122bc2679848d1cb93c254
Joseph Devereaux Libatique <me@jdlibatique.dev>
2025-06-12 22:27:09 +0800

    Add handling for out of bounds asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,15 +1,17 @@
+import logging
 import random
 
 import pygame
 
 from circleshape import CircleShape
-from constants import ASTEROID_MIN_RADIUS
+from constants import ASTEROID_MIN_RADIUS, ASTEROID_MAX_OUT_OF_BOUNDS_TIME
 
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
         self.rotation = 0
+        self.out_of_bounds_time = 0
 
     def draw(self, screen):
         pygame.draw.circle(
@@ -19,8 +21,18 @@ class Asteroid(CircleShape):
             self.radius, 2
         )
 
-    def update(self, dt):
-        self.position += self.velocity * dt
+    def update(self, delta_time):
+        self.position += self.velocity * delta_time
+
+        if self.is_out_of_bounds():
+            self.out_of_bounds_time += delta_time
+            if self.out_of_bounds_time > ASTEROID_MAX_OUT_OF_BOUNDS_TIME:
+                logging.info("Asteroid out of bounds, removing asteroid.")
+                self.kill()
+
+    def is_out_of_bounds(self):
+        return (self.position.x < 0 or self.position.x >= pygame.display.get_surface().get_width()
+                or self.position.y < 0 or self.position.y >= pygame.display.get_surface().get_height())
 
     def split(self):
         self.kill()
--- player.py
+++ player.py
@@ -36,31 +36,31 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
 
-    def rotate(self, dt):
-        self.rotation += (PLAYER_TURN_SPEED * dt)
+    def rotate(self, delta_time):
+        self.rotation += (PLAYER_TURN_SPEED * delta_time)
 
-    def move(self, dt):
+    def move(self, delta_time):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
+        self.position += forward * PLAYER_SPEED * delta_time
 
     def shoot(self):
         shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
-    def update(self, dt):
-        self.timer -= dt
+    def update(self, delta_time):
+        self.timer -= delta_time
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotate(-dt)
+            self.rotate(-delta_time)
         if keys[pygame.K_d]:
-            self.rotate(dt)
+            self.rotate(delta_time)
 
         if keys[pygame.K_w]:
-            self.move(dt)
+            self.move(delta_time)
 
         if keys[pygame.K_s]:
-            self.move(-dt)
+            self.move(-delta_time)
 
         if keys[pygame.K_SPACE]:
             if self.timer > 0:
